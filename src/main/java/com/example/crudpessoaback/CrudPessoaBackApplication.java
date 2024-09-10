5209d6b6274f3a4c1321b1304b6f7df9ddfcf48e
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-09-10 10:19:37 -0300

    Criação do projeto + liberação de requisições (corsOrigin)

---
+++ src/main/java/com/example/crudpessoaback/CrudPessoaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudpessoaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPessoaBackApplication.class, args);
+	}
+
+}
